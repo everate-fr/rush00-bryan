6ee607660d0972c94e2b431bfcc83a3fa800bea1
wshloic <lbertran@student.42lyon.fr>
2023-09-16 14:47:24 +0200

    feat: send login request when clicking on login button

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { IAuthenticationService } from './services/authentication/iauthentication.service';
+import { AuthenticationService } from './services/authentication/authentication.service';
+import { IApiService } from './services/api/iapi.service';
+import { ApiService } from './services/api/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +18,14 @@ import { LoginComponent } from './pages/auth/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: IAuthenticationService, useClass: AuthenticationService },
+    { provide: IApiService, useClass: ApiService }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front/src/app/pages/auth/login/login.component.ts
+++ front/src/app/pages/auth/login/login.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { IAuthenticationService } from 'src/app/services/authentication/iauthentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  public passwordShowed: boolean = false;
 
-  public showPassword() {
-    this.passwordShowed = !this.passwordShowed;
-  }
+	public passwordShowed: boolean = false;
+
+	public invalidCredentials: boolean = false;
+	public userName: string = '';
+	public password: string = '';
+
+	constructor(
+		private readonly _authenticationService: IAuthenticationService,
+	) { }
+
+	public showPassword() {
+		this.passwordShowed = !this.passwordShowed;
+	}
+
+	public async validateForm() {
+		const response = await this._authenticationService.login(this.userName, this.password)
+		console.log(response);
+		
+		return true;
+	}
 }
