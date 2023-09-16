fe595a4561aa59bb1f92766c589c2d6960930bf3
wshloic <lbertran@student.42lyon.fr>
2023-09-16 16:02:16 +0200

    feat: redirect to home on login success

---
+++ front/src/app/enum/path.enum.ts
@@ -0,0 +1,4 @@
+export enum PathEnum {
+    Login = 'login',
+    Home = 'home',
+}
--- front/src/app/pages/auth/login/login.component.ts
+++ front/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PathEnum } from 'src/app/enum/path.enum';
 import { IAuthenticationService } from 'src/app/services/authentication/iauthentication.service';
 
 @Component({
@@ -11,21 +13,31 @@ export class LoginComponent {
 	public passwordShowed: boolean = false;
 
 	public invalidCredentials: boolean = false;
+	public disableSubmit: boolean = false;
 	public userName: string = '';
 	public password: string = '';
 
 	constructor(
 		private readonly _authenticationService: IAuthenticationService,
+		private readonly _router: Router,
 	) { }
 
-	public showPassword() {
+	public showPassword(): void {
 		this.passwordShowed = !this.passwordShowed;
 	}
 
-	public async validateForm() {
-		const response = await this._authenticationService.login(this.userName, this.password)
-		console.log(response);
-		
-		return true;
+	public async validateForm(): Promise<void> {
+		this.invalidCredentials = false;
+		this.disableSubmit = true;
+
+		const isValid = await this._authenticationService.login(this.userName, this.password)
+
+		this.disableSubmit = false;
+
+		if (isValid) {
+			this._router.navigate([PathEnum.Home]);
+		} else {
+			this.invalidCredentials = true;
+		}
 	}
 }
