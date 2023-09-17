c5d13be9003a6cd444c29d9e14ef3e3d65c17c65
Olivier Cartier <me@cestoliv.com>
2023-09-17 13:12:29 +0200

    feat: ✨ crud for emotions

--- backend/src/emotions/emotions.controller.ts
+++ backend/src/emotions/emotions.controller.ts
@@ -1,16 +1,55 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import {
+	Controller,
+	Post,
+	Body,
+	UseGuards,
+	Req,
+	Get,
+	Patch,
+	Param,
+	Delete,
+} from '@nestjs/common';
 
 import { EmotionsService } from './emotions.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { CreateEmotionDto } from './dto/create-emotion.dto';
+import { UpdateEmotionDto } from './dto/update-emotion.dto';
 
 @Controller('/api/v1/emotions')
 export class EmotionsController {
 	constructor(private readonly emotionsService: EmotionsService) {}
 
+	@Get('/')
+	@UseGuards(JwtAuthGuard)
+	findAll(@Req() request) {
+		return this.emotionsService.findAll(request.user);
+	}
+
+	@Get('/:date')
+	@UseGuards(JwtAuthGuard)
+	findByDate(@Req() request, @Param('date') date: string) {
+		return this.emotionsService.findByDate(date, request.user);
+	}
+
 	@Post('/')
 	@UseGuards(JwtAuthGuard)
 	create(@Req() request, @Body() createEmotionDto: CreateEmotionDto) {
 		return this.emotionsService.create(createEmotionDto, request.user);
+	}
+
+	@Patch('/')
+	@UseGuards(JwtAuthGuard)
+	update(
+		@Req() request,
+		@Body() updateEmotionDto: UpdateEmotionDto,
+		@Param('id') id: number,
+	) {
+		return this.emotionsService.update(id, updateEmotionDto, request.user);
+	}
+
+	@Delete('/date')
+	@UseGuards(JwtAuthGuard)
+	delete(@Req() request, @Param('id') id: number) {
+		return this.emotionsService.delete(id, request.user);
 	}
 }
--- backend/src/emotions/emotions.service.ts
+++ backend/src/emotions/emotions.service.ts
@@ -6,6 +6,7 @@ import { CreateEmotionDto } from './dto/create-emotion.dto';
 import { EntriesService } from 'src/entries/entries.service';
 import { User } from 'src/users/entities/user.entity';
 import { Emotion } from './entities/emotion.entity';
+import { UpdateEmotionDto } from './dto/update-emotion.dto';
 
 @Injectable()
 export class EmotionsService {
@@ -33,5 +34,40 @@ export class EmotionsService {
 		emotion.journal = createEmotionDto.journal;
 
 		return this.emotionsRepository.save(emotion);
+	}
+
+	async findAll(user: User) {
+		return this.emotionsRepository.find({
+			where: { entry: { user: { id: user.id } } },
+			order: { entry: { date: 'DESC' } },
+		});
+	}
+
+	async findByDate(date: string, user: User) {
+		return this.emotionsRepository.find({
+			where: { entry: { user: { id: user.id }, date: date } },
+		});
+	}
+
+	async update(id: number, updateEmotionDto: UpdateEmotionDto, user: User) {
+		const emotion = await this.emotionsRepository.findOne({
+			where: { id: id, entry: { user: { id: user.id } } },
+		});
+		if (!emotion) throw new BadRequestException('Emotion not found');
+
+		emotion.emotion = updateEmotionDto.emotion;
+		emotion.reason = updateEmotionDto.reason;
+		emotion.journal = updateEmotionDto.journal;
+
+		return this.emotionsRepository.save(emotion);
+	}
+
+	async delete(id: number, user: User) {
+		const emotion = await this.emotionsRepository.findOne({
+			where: { id: id, entry: { user: { id: user.id } } },
+		});
+		if (!emotion) throw new BadRequestException('Emotion not found');
+
+		return this.emotionsRepository.delete(emotion);
 	}
 }
